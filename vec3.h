adce09245d68d0ba3f719e6187efabf33bce4877
Charlie <charlie_xumeng@hotmail.com>
2024-12-10 18:30:38 +1030

    save

---
+++ vec3.h
@@ -0,0 +1,60 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <iostream>
+#include <cmath>
+
+class vec3
+{
+public:
+	double e[3];
+
+	// Constructors
+	vec3() : e{0, 0, 0} {}
+	vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+	// Getters for x, y, z
+	double x() const { return e[0]; }
+	double y() const { return e[1]; }
+	double z() const { return e[2]; }
+
+	// Overloaded operators
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	double operator[](int i) const { return e[i]; }
+	double &operator[](int i) { return e[i]; }
+
+	vec3 &operator+=(const vec3 &v)
+	{
+		e[0] += v.e[0];
+		e[1] += v.e[1];
+		e[2] += v.e[2];
+		return *this;
+	}
+
+	vec3 &operator*=(const double t)
+	{
+		e[0] *= t;
+		e[1] *= t;
+		e[2] *= t;
+		return *this;
+	}
+
+	vec3 &operator/=(const double t)
+	{
+		return *this *= 1 / t;
+	}
+
+	// root of x^2 + y^2 + z^2
+	double length() const
+	{
+		return sqrt(length_squared());
+	}
+
+	// x^2 + y^2 + z^2
+	double length_squared() const
+	{
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+};
+
+#endif
